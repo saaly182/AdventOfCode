ef7734e7198e4d0946fd77ac4f928818a8a423db
John <65102131+saaly182@users.noreply.github.com>
2022-12-04 10:31:26 -0800

    Refactor day 1

--- 2022/d01/d01.py
+++ 2022/d01/d01.py
@@ -10,22 +10,21 @@ use_sample = False
 SAMPLE_INPUTFILE = './sample_input.txt'
 INPUTFILE = './input.txt'
 
+
 def make_cal_list(fname):
   elfcalories = []
-  for record in multiline_file('input.txt'):
+  for record in multiline_file(fname):
     cals = sum([int(x) for x in record.split()])
     elfcalories.append(cals)
   return elfcalories
 
 
-def part1(fname):
-  return max(make_cal_list(fname))
+def part1(cal_list):
+  return max(cal_list)
 
 
-def part2(fname):
-  cal_list = make_cal_list(fname)
-  cal_list.sort(reverse=True)
-  return sum(cal_list[:3])
+def part2(cal_list):
+  return sum(sorted(cal_list, reverse=True)[:3])
 
 
 def main():
@@ -33,8 +32,9 @@ def main():
   if use_sample:
     fname = SAMPLE_INPUTFILE
 
-  print("Part 1 answer =", part1(fname))
-  print("Part 2 answer =", part2(fname))
+  cal_list = make_cal_list(fname)
+  print("Part 1 answer =", part1(cal_list))
+  print("Part 2 answer =", part2(cal_list))
 
 
 if __name__ == '__main__':
