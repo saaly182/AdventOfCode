3114a937b96ac9951f1cf9ad23d6a2ed10a62846
John <65102131+saaly182@users.noreply.github.com>
2022-12-04 22:54:11 -0800

    Soln d5 p1

---
+++ 2022/d05/d05.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3 -u
+
+import re
+
+debug = False
+use_sample = False
+SAMPLE_INPUTFILE = './sample_input.txt'
+INPUTFILE = './input.txt'
+
+
+def genstacks(slines):
+  # info is easier to parse in transposed form with a stack on each line
+  tlines = [''.join(chars).strip() for chars in zip(*reversed(slines))]
+  stacks = {}
+  for s in tlines:
+    if re.search('[A-Z]', s):
+      stacks[int(s[0])] = list(s[1:])
+
+  return stacks
+
+
+def genmoves(mlines):
+  moves = []
+  for m in mlines:
+    moves.append(tuple(int(x) for x in m.split()[1::2]))
+  return tuple(moves)
+
+
+def parse_input(fname):
+  "Return the stacks and moves in the input file."
+  stacklines = []
+  movelines = []
+  stackmode = True
+  with open(fname) as file:
+    for line in file:
+
+      # blank line separates stack and move data
+      if line == '\n':
+        stackmode = False
+        continue
+
+      if stackmode == True:
+        stacklines.append(line)
+      else:
+        movelines.append(line)
+
+  return genstacks(stacklines), genmoves(movelines)
+
+
+def part1(stacks, moves):
+  for count, src, dst in moves:
+    for i in range(count):
+      stacks[dst].append(stacks[src].pop())
+  msg = []
+  for sk in sorted(stacks.keys()):
+    msg.append(stacks[sk][-1])
+  return ''.join(msg)
+
+
+def part2():
+  return None
+
+
+def main():
+  fname = INPUTFILE
+  if use_sample:
+    fname = SAMPLE_INPUTFILE
+
+  stacks, moves = parse_input(fname)
+
+  print("Part 1 answer =", part1(stacks, moves))
+  print("Part 2 answer =", part2())
+
+
+if __name__ == '__main__':
+  main()
