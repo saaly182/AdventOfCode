29ba6d82603f42c327afcdd664c6269f5b6fb3c0
John <65102131+saaly182@users.noreply.github.com>
2022-12-10 12:03:04 -0800

    Get rid of several DRY violations

--- 2022/d10/d10.py
+++ 2022/d10/d10.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3 -u
 
+from collections import namedtuple
+
+Instruction = namedtuple('Instruction', "opcode operand nticks")
+
 
 def part1(prog):
   cycle = 0
   regx = 1
   sigsum = 0
-  cycchecks = set(range(20, 221, 40))
 
   for inst in prog:
-    cycle += 1
-
-    if cycle in cycchecks:
-      sigsum += cycle * regx
-
-    if inst[0] == 'addx':
+    for tick in range(inst.nticks):
       cycle += 1
-      if cycle in cycchecks:
+      if (cycle - 20) % 40 == 0:
         sigsum += cycle * regx
+
+    if inst.opcode == 'addx':
       regx += inst[1]
 
   return sigsum
@@ -28,34 +28,25 @@ def pixel(cycle):
   return row, col
 
 
-def showcrt(crt):
-  for line in crt:
-    print(''.join(line))
-
-
 def part2(prog):
   cycle = 0
   regx = 1
   crt = [[' '] * 40 for _ in range(6)]
 
-  def updatecrt():
-    nonlocal cycle
-    row, col = pixel(cycle)
-    sprite = (regx - 1, regx, regx + 1)
-    if col in sprite:
-      crt[row][col] = '#'
-    cycle += 1
-
   for inst in prog:
-    updatecrt()
+    for tick in range(inst.nticks):
+      row, col = pixel(cycle)
+      sprite = (regx - 1, regx, regx + 1)
+      if col in sprite:
+        crt[row][col] = '#'
+      cycle += 1
 
-    if inst[0] == 'addx':
-      updatecrt()
-      regx += inst[1]
+    if inst.opcode == 'addx':
+      regx += inst.operand
 
-  showcrt(crt)
+  final_display = '\n'.join([''.join(row) for row in crt])
 
-  return None
+  return final_display
 
 
 def main():
@@ -65,13 +56,14 @@ def main():
       line = line.rstrip()
       toks = line.split()
       if toks[0] == 'noop':
-        prog.append(tuple(toks))
+        prog.append(Instruction('noop', None, 1))
+      elif toks[0] == 'addx':
+        prog.append(Instruction('addx', int(toks[1]), 2))
       else:
-        prog.append((toks[0], int(toks[1])))
-
+        raise ValueError
 
   print("Part 1 answer =", part1(prog))
-  print("Part 2 answer =", part2(prog))
+  print("Part 2 answer =\n", part2(prog))
 
 
 if __name__ == '__main__':
