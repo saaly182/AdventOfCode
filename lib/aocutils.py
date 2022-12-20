d86d53fa550012a64b1f32dbe10bbf4507225609
John <65102131+saaly182@users.noreply.github.com>
2022-12-19 22:20:41 -0800

    Add util fnc for merge_intervals

--- lib/aocutils.py
+++ lib/aocutils.py
@@ -3,6 +3,7 @@
 Various utility functions for AoC
 """
 
+import copy
 import unittest
 
 
@@ -18,6 +19,27 @@ def drange(a, b):
   return dr
 
 
+def merge_intervals(input_intervals):
+  """
+  Return a list of the merged set of the input intervals.
+
+  Input: an iterable of [a, b] intervals, where a & b are ints and a <= b
+  Based on https://www.geeksforgeeks.org/merging-intervals/
+  """
+  intervals = copy.deepcopy(input_intervals)  # don't change the input
+  for i in intervals:
+    assert len(i) == 2 and (i[0] <= i[1])
+  intervals.sort()
+  stack = []
+  stack.append(intervals[0])
+  for i in intervals[1:]:
+    if stack[-1][0] <= i[0] <= stack[-1][1]:
+      stack[-1][1] = max(stack[-1][1], i[1])
+    else:
+      stack.append(i)
+  return stack
+
+
 class TestDrange(unittest.TestCase):
   def test_drange(self):
     x = list(drange(3, 6))
@@ -26,6 +48,15 @@ class TestDrange(unittest.TestCase):
     self.assertEqual(x, [7, 6, 5, 4, 3, 2])
     x = list(drange(9, 9))
     self.assertEqual(x, [9])
+
+class TestMergeIntervals(unittest.TestCase):
+  def test_merge_intervals(self):
+    i1 = [[10, 20], [1, 5], [11, 21], [5, 8], [0, 1]]
+    i2 = merge_intervals(i1)
+    self.assertEqual(i2, [[0, 8], [10, 21]])
+    i1 = [[10, 20], [-1, 5], [6, 6], [90, 99], [91, 98]]
+    i2 = merge_intervals(i1)
+    self.assertEqual(i2, [[-1, 5], [6, 6], [10, 20], [90, 99]])
 
 
 if __name__ == "__main__":
