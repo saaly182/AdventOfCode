97bb161ba1abc52a397cb6fc8c136a15bd87f516
John <65102131+saaly182@users.noreply.github.com>
2023-01-15 11:30:41 -0800

    Switch to 4-space indents

--- lib/aoc_skel.py
+++ lib/aoc_skel.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3 -u
 
 def part1():
-  return None
+    return None
 
 
 def part2():
-  return None
+    return None
 
 
 def slurp(fname):
-  with open(fname) as file:
-    return [line.rstrip() for line in file.readlines()]
+    with open(fname) as file:
+        return [line.rstrip() for line in file.readlines()]
 
 
 def main():
-  sample_input = slurp('sample_input.txt')
-  main_input = slurp('input.txt')
+    sample_input = slurp('sample_input.txt')
+    main_input = slurp('input.txt')
 
-  for inp in (sample_input, main_input):
-    print("Part 1 answer =", part1())
-    print("Part 2 answer =", part2())
-    print()
+    for inp in (sample_input, main_input):
+        print("Part 1 answer =", part1())
+        print("Part 2 answer =", part2())
+        print()
 
 
 if __name__ == '__main__':
-  main()
+    main()
