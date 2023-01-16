575d3fc2320e8811d7c19d8279e5ad0e014b02fb
John <65102131+saaly182@users.noreply.github.com>
2023-01-15 19:42:33 -0800

    Soln d24 p1

---
+++ 2022/d24/d24.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3 -u
+
+import functools
+import math
+
+class Valley:
+    # class vars
+    drdc = {'<': (0, -1), '>': (0, 1), '^': (-1, 0), 'v': (1, 0)}
+
+    def __init__(self, inp):
+        self.maxrow = len(inp) - 1
+        self.maxcol = len(inp[0]) - 1
+        self.start = (0, inp[0].index('.'))
+        self.end = (self.maxrow, inp[-1].index('.'))
+        self.blizzards = self._gen_blizzards(inp)
+        self._visited_states = set()
+        self._lcm = math.lcm(self.maxrow - 1, self.maxcol - 1)  # least common multiple of row & col data sizes
+
+    @staticmethod
+    def _gen_blizzards(inp):
+        blizlist = []
+        for r0, line in enumerate(inp):
+            for c0, sym in enumerate(line):
+                if sym in set('<>^v'):
+                    blizlist.append((sym, r0, c0))
+        return tuple(blizlist)
+
+    def _blizpos(self, b, t):
+        """Return the row, col position of blizzard b at time t"""
+        # This was a pain to work out correctly.
+        if t < 0:
+            raise ValueError
+        sym, r0, c0 = b
+        t_rmod = t % (self.maxrow - 1)
+        t_cmod = t % (self.maxcol - 1)
+        r = (r0 - 1 + (t_rmod * Valley.drdc[sym][0])) % (self.maxrow - 1) + 1
+        c = (c0 - 1 + (t_cmod * Valley.drdc[sym][1])) % (self.maxcol - 1) + 1
+        return r, c
+
+    @functools.lru_cache(maxsize=None)
+    def _blizlocs(self, t):
+        """Return the set of cells occupied by blizzards at time t."""
+        blizcells = set()
+        for b in self.blizzards:
+            blizcells.add(self._blizpos(b, t))
+        return blizcells
+
+    def _visited(self, eloc, t):
+        """Return true if we've visited this state before."""
+        # The state of the blizzards is the same every self._lcm minutes, so if we see that state with the same
+        # expedition location again, we have already visited this configuration.
+        tmod = t % self._lcm
+        if (eloc, tmod) in self._visited_states:
+            return True
+        else:
+            self._visited_states.add((eloc, tmod))
+        return False
+
+    def bfs(self):
+        """Search for self.end and return the time it took."""
+        check_count = 0
+        bq = [(self.start, 0)]  # bfs queue; elements are (expedition location, time)
+        while bq:
+            check_count += 1
+            eloc, t1 = bq.pop(0)
+            er, ec = eloc
+            if eloc == self.end:
+                return t1
+            t2 = t1 + 1
+            bcells = self._blizlocs(t2)
+            for d in list(Valley.drdc.values()) + [(0, 0)]:
+                r = er + d[0]
+                c = ec + d[1]
+                if c < 1 or c == self.maxcol:
+                    continue
+                if (r < 1 or r == self.maxrow) and ((r, c) not in (self.start, self.end)):
+                    continue
+                if (r, c) not in bcells and not self._visited((r, c), t2):
+                    bq.append(((r, c), t2))
+
+
+def part1(inp):
+    valley = Valley(inp)
+    return valley.bfs()
+
+
+def part2():
+    return None
+
+
+def slurp(fname):
+    with open(fname) as file:
+        return [line.rstrip() for line in file.readlines()]
+
+
+def main():
+    sample_input = slurp('sample_input.txt')
+    main_input = slurp('input.txt')
+
+    for inp in (sample_input, main_input):
+        print("Part 1 answer =", part1(inp))
+        print("Part 2 answer =", part2())
+        print()
+
+
+if __name__ == '__main__':
+    main()
