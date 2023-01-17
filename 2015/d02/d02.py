0182d1e3332871e35072fde35cf5b4f894d688fa
John <65102131+saaly182@users.noreply.github.com>
2023-01-16 17:43:08 -0800

    Soln 2015 d02

---
+++ 2015/d02/d02.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3 -u
+
+def part1(inp):
+    paper_needed = 0
+    for dims in inp:
+        l, w, h = (int(x) for x in dims.split('x'))
+        areas = l * w, l * h, w * h
+        paper_needed += 2 * (sum(areas)) + min(areas)
+    return paper_needed
+
+
+def part2(inp):
+    ribbon_needed = 0
+    for dims in inp:
+        l, w, h = (int(x) for x in dims.split('x'))
+        perims = 2 * (l + w), 2 * (l + h), 2 * (w + h)
+        volume = l * w * h
+        ribbon_needed += min(perims) + volume
+    return ribbon_needed
+
+
+def slurp(fname):
+    with open(fname) as file:
+        return [line.rstrip() for line in file.readlines()]
+
+
+def main():
+    sample_input = slurp('sample_input.txt')
+    main_input = slurp('input.txt')
+
+    for inp in (sample_input, main_input):
+        print("Part 1 answer =", part1(inp))
+        print("Part 2 answer =", part2(inp))
+        print()
+
+
+if __name__ == '__main__':
+    main()
