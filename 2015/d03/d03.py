ae4ee1c2bc9e01fe1463dd0fc31ea40e69e686f3
John <65102131+saaly182@users.noreply.github.com>
2023-01-19 23:09:41 -0800

    Soln 2015 d03

---
+++ 2015/d03/d03.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3 -u
+
+import collections
+
+move_incr = {'<': (-1, 0), '>': (1, 0), '^': (0, 1), 'v': (0, -1)}
+
+
+def part1(moves):
+    pos = (0, 0)
+    house2presents = collections.defaultdict(int)
+    house2presents[pos] = 1
+    for m in moves:
+        if m not in move_incr:
+            raise ValueError(m)
+        dx, dy = move_incr[m]
+        pos = pos[0] + dx, pos[1] + dy
+        house2presents[pos] += 1
+    return len(house2presents)
+
+
+def part2(moves):
+    house2presents = collections.defaultdict(int)
+    house2presents[(0, 0)] = 2
+    for offset in (0, 1):
+        pos = (0, 0)
+        for m in moves[offset::2]:
+            if m not in move_incr:
+                raise ValueError(m)
+            dx, dy = move_incr[m]
+            pos = pos[0] + dx, pos[1] + dy
+            house2presents[pos] += 1
+    return len(house2presents)
+
+
+def slurp(fname):
+    with open(fname) as file:
+        return [line.rstrip() for line in file.readlines()]
+
+
+def main():
+    main_input = slurp('input/input.txt')
+
+    for inp in (main_input,):
+        moves = inp[0]
+        print("Part 1 answer =", part1(moves))
+        print("Part 2 answer =", part2(moves))
+        print()
+
+
+if __name__ == '__main__':
+    main()
