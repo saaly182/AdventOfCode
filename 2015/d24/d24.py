bf50c91038c6d3947e1f13ba0ceb960057e0696a
John <65102131+saaly182@users.noreply.github.com>
2023-02-05 22:35:53 -0800

    Soln 2015 d24 p2

--- 2015/d24/d24.py
+++ 2015/d24/d24.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3 -u
 
-# ref: https://en.wikipedia.org/wiki/Multiway_number_partitioning
+# FYI, pypy3 ran in 1/12th the time of python3 for this.
 
-import functools
 import itertools
 import math
 
 
-def tupsub(a, b):
-    """Return tuple_a - tuple_b."""
+def tuple_subtract(a, b):
+    """Like set subtraction, but for tuples. Basically multiset subtraction."""
     al = list(a)
     bl = list(b)
     for v in bl:
@@ -19,8 +18,7 @@ def tupsub(a, b):
 
 # https://en.wikipedia.org/wiki/Partition_problem
 # https://www.geeksforgeeks.org/partition-problem-dp-18/
-# recursive
-@functools.cache
+# NOTE: This could use @functools.cache, but it blows up memory for part2.
 def can_partition(arr, n, tsum):
     # tsum is the "target sum"
     if tsum == 0:
@@ -28,28 +26,33 @@ def can_partition(arr, n, tsum):
     if n == 0:
         return False
     if arr[n - 1] > tsum:
-        # if an individual elem is > tsum, must be False
+        # if an individual elem is > tsum, no soln can exist
         return False
     return (can_partition(arr, n - 1, tsum) or
             can_partition(arr, n - 1, tsum - arr[n - 1]))
 
 
+def weight_groups(weights, size, tsum):
+    """Generate subgroups of the given size that sum to the target sum."""
+    for wg in itertools.combinations(weights, size):
+        if sum(wg) == tsum:
+            yield wg
+
+
 def part1(weights):
     wsum = sum(weights)
     if wsum % 3 != 0:
-        raise ValueError('cannot divide into three groups')
+        raise ValueError(f'cannot divide into three equal-weight groups')
     gw = wsum // 3  # group weight
 
     qe = []  # quantum entanglements
     for g1size in range(1, len(weights) - 1):
         if qe:
             break  # no need to search larger g1's
-        for g1 in itertools.combinations(weights, g1size):
-            if sum(g1) != gw:
-                continue
+        for g1 in weight_groups(weights, g1size, gw):
             # Now we're down to the classic Partition Problem with the
             # remaining weights.
-            weights_left = tupsub(weights, g1)
+            weights_left = tuple_subtract(weights, g1)
             wlsum = sum(weights_left)
             if wlsum % 2 != 0:
                 continue
@@ -59,8 +62,38 @@ def part1(weights):
     return min(qe) if qe else None
 
 
-def part2():
-    return None
+def part2(weights):
+    # NOTE: I'm intentionally not trying to refactor the code to avoid DRY
+    # between part1 and part2. Going for minimized effort on this one for now.
+    # TODO: consider refactoring to combine part1 and part2.
+    # ref: https://en.wikipedia.org/wiki/Multiway_number_partitioning
+    wsum = sum(weights)
+    if wsum % 4 != 0:
+        raise ValueError(f'cannot divide into four equal-weight groups')
+    gw = wsum // 4  # group weight
+
+    qe = []  # quantum entanglements
+    for g1size in range(1, len(weights) - 2):
+        if qe:
+            break  # no need to search larger g1's
+        for g1 in weight_groups(weights, g1size, gw):
+            weights_left1 = tuple_subtract(weights, g1)
+            wlsum1 = sum(weights_left1)
+            if wlsum1 % 3 != 0:
+                continue
+            for g2size in range(1, len(weights_left1) - 1):
+                for g2 in weight_groups(weights_left1, g2size, gw):
+                    # Now we're down to the classic Partition Problem with the
+                    # remaining weights.
+                    weights_left2 = tuple_subtract(weights_left1, g2)
+                    wlsum2 = sum(weights_left2)
+                    if wlsum2 % 2 != 0:
+                        continue
+                    if can_partition(weights_left2, len(weights_left2),
+                                     wlsum2 // 2):
+                        qe.append(math.prod(g1))
+
+    return min(qe) if qe else None
 
 
 def slurp(fname):
@@ -75,7 +108,7 @@ def main():
     for inp in (sample_input, main_input):
         weights = tuple(int(x) for x in inp)
         print("Part 1 answer =", part1(weights))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(weights))
         print()
 
 
