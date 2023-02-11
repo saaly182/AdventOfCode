ecbdf153e8ae8595c43a4b90da191d2671c5976d
John <65102131+saaly182@users.noreply.github.com>
2023-02-11 13:42:53 -0800

    Soln 2016 d07 p2

--- 2016/d07/d07.py
+++ 2016/d07/d07.py
@@ -27,6 +27,31 @@ def supports_tls(ip):
     return st
 
 
+def supports_ssl(ip):
+    # need to use lookahead assertion (?=) to find overlapping results
+    aba = r'(?=(([a-z]).\2))'
+    supernet_abas = set()
+    hypernet_abas = set()
+
+    for token in tokenize(ip):
+        mos = re.finditer(aba, token)
+        for mo in mos:
+            s = mo.group(1)
+            if s[0] == s[1]:  # this is an 'aaa' not an 'aba'
+                continue
+            if token[0] == '[':
+                hypernet_abas.add(s)
+            else:
+                supernet_abas.add(s)
+
+    for aba in supernet_abas:
+        x, y = aba[:2]
+        if ''.join((y, x, y)) in hypernet_abas:
+            return True
+
+    return False
+
+
 def part1(inp):
     tls_count = 0
     for ip in inp:
@@ -35,8 +60,12 @@ def part1(inp):
     return tls_count
 
 
-def part2():
-    return None
+def part2(inp):
+    ssl_count = 0
+    for ip in inp:
+        if supports_ssl(ip):
+            ssl_count += 1
+    return ssl_count
 
 
 def slurp(fname):
@@ -50,7 +79,7 @@ def main():
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(inp))
         print()
 
 
