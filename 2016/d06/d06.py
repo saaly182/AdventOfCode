cbad685f7bf2bb149bd48353fe783f98a46a1442
John <65102131+saaly182@users.noreply.github.com>
2023-02-11 10:10:06 -0800

    Soln 2016 d06 p2

--- 2016/d06/d06.py
+++ 2016/d06/d06.py
@@ -3,7 +3,7 @@
 import collections
 
 
-def part1(inp):
+def part1(inp, select_min=False):
     # This approach may not be the most efficient
     cols = len(inp[0])
     charcounts = [collections.defaultdict(int) for _ in range(cols)]
@@ -14,9 +14,14 @@ def part1(inp):
             charcounts[col][char] += 1
 
     realmsg = []
+    if select_min:
+        tgtfnc = min
+    else:
+        tgtfnc = max
+
     for col in range(cols):
-        maxfreq = max(charcounts[col].values())
-        char = [c for c in charcounts[col] if charcounts[col][c] == maxfreq]
+        tgtfreq = tgtfnc(charcounts[col].values())
+        char = [c for c in charcounts[col] if charcounts[col][c] == tgtfreq]
         if len(char) != 1:
             raise ValueError(f"multiple chars have max freq {char}")
         realmsg.append(char[0])
@@ -24,8 +29,8 @@ def part1(inp):
     return ''.join(realmsg)
 
 
-def part2():
-    return None
+def part2(inp):
+    return part1(inp, select_min=True)
 
 
 def slurp(fname):
@@ -39,7 +44,7 @@ def main():
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(inp))
         print()
 
 
