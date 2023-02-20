88bf6cdae2bd18d4b00301c96d3f34ccf05def86
John <65102131+saaly182@users.noreply.github.com>
2023-02-19 17:40:08 -0800

    Soln 2016 d16 p1

---
+++ 2016/d16/d16.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3 -u
+
+def filldisk(disklen, init_data):
+    data = list(init_data)
+
+    while len(data) < disklen:
+        b = list(reversed(data))
+        for i, c in enumerate(b):
+            b[i] = '0' if c == '1' else '1'
+        data.append('0')
+        data.extend(b)
+
+    return ''.join(data[:disklen])
+
+
+def checksum(s):
+    csum = list(s)
+    while len(csum) % 2 == 0:
+        next_csum = []
+        for a, b in zip(csum[::2], csum[1::2]):
+            bit = '1' if a == b else '0'
+            next_csum.append(bit)
+        csum = next_csum
+    return ''.join(csum)
+
+
+def part1(disklen, init_data):
+    return checksum(filldisk(disklen, init_data))
+
+
+def part2():
+    return None
+
+
+def main():
+    sample_input = (20, '10000')
+    main_input = (272, '10011111011011001')
+
+    for inp in (sample_input, main_input):
+        print("Part 1 answer =", part1(*inp))
+        print("Part 2 answer =", part2())
+        print()
+
+
+if __name__ == '__main__':
+    main()
