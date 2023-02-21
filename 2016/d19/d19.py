b33c120bf95142cdfaf12b44ea401f9a0db25b62
John <65102131+saaly182@users.noreply.github.com>
2023-02-20 20:18:49 -0800

    Soln 2016 d19 p2

--- 2016/d19/d19.py
+++ 2016/d19/d19.py
@@ -7,39 +7,80 @@ import dataclasses
 class Elf:
     presents: int
     next_elf: int
+    prev_elf: int
 
 
-def winner(nelves):
-    """Return the number of the elf that ends up with everything."""
-    # elf is a linked-list ring
+def elf_ring(nelves):
+    # Doubly-linked ring
     elf = {}
-    for i in range(1, nelves):
-        elf[i] = Elf(1, i + 1)
-    elf[nelves] = Elf(1, 1)
+    for i in range(2, nelves):
+        elf[i] = Elf(1, i + 1, i - 1)
+    elf[1] = Elf(1, 2, nelves)
+    elf[nelves] = Elf(1, 1, nelves - 1)
+    return elf
+
+
+def elf_unlink(elf, i):
+    n = elf[i].next_elf
+    p = elf[i].prev_elf
+    elf[n].prev_elf = p
+    elf[p].next_elf = n
+    del elf[i]
+
+
+def elf_across(elist, i):
+    """Return the elf num & idx of the elf across the circle from i."""
+    i_loc = elist.index(i)
+    j_loc = (i_loc + len(elist) // 2) % len(elist)
+    return elist[j_loc], j_loc
+
+
+def winner_part1(nelves):
+    """Return the number of the elf that ends up with everything."""
+    elf = elf_ring(nelves)
 
     i = 1
     while True:
-        if elf[i].presents == 0:
-            i = i % nelves + 1
-            continue
         if elf[i].presents == nelves:
             # This elf has all the presents
             return i
-        # take all presents from the next elf with presents
+        # take all presents from the next elf
         j = elf[i].next_elf
-        while elf[j].presents == 0:
-            j = elf[j].next_elf
         elf[i].presents += elf[j].presents
-        elf[j].presents = 0
-        i = i % nelves + 1
+        elf_unlink(elf, j)
+        i = elf[i].next_elf
+
+
+def winner_part2(nelves):
+    # This is slow... Took 30 minutes using pypy3 on my machine.
+    # Didn't find a faster way to handle the "across the circle"
+    # aspect of part 2.
+    elf = elf_ring(nelves)
+    elist = sorted(elf)
+
+    # Unlike in part1, in this part we're actively deleting elves who
+    # drop to zero presents.
+    i = 1
+    while True:
+        if elf[i].presents == nelves:
+            # This elf has all the presents
+            return i
+        # take from the elf across the circle
+        j, j_idx = elf_across(elist, i)
+        elf[i].presents += elf[j].presents
+        del elist[j_idx]
+        elf_unlink(elf, j)
+        if len(elist) % 10_000 == 0:
+          print(f'{len(elist)=}')
+        i = elf[i].next_elf
 
 
 def part1(nelves):
-    return winner(nelves)
+    return winner_part1(nelves)
 
 
-def part2():
-    return None
+def part2(nelves):
+    return winner_part2(nelves)
 
 
 def main():
@@ -48,7 +89,7 @@ def main():
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(inp))
         print()
 
 
