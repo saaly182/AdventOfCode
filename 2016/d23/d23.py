440fd99e5d4bbe19e9f19c24fdc2bd01b02fe4a8
John <65102131+saaly182@users.noreply.github.com>
2023-02-24 23:49:37 -0800

    Soln 2016 d23 p2

--- 2016/d23/d23.py
+++ 2016/d23/d23.py
@@ -4,10 +4,11 @@ import copy
 
 
 def decode(mem):
-    inst = mem[0]
-    op1 = mem[1]
-    op2 = mem[2] if len(mem) == 3 else None
-    return inst, op1, op2
+    protected = mem[0]  # bool; true means tgl cannot modify this instruction
+    inst = mem[1]
+    op1 = mem[2] if len(mem) > 2 else None
+    op2 = mem[3] if len(mem) == 4 else None
+    return protected, inst, op1, op2
 
 
 def tgl_inst(program, addr):
@@ -18,7 +19,11 @@ def tgl_inst(program, addr):
     if addr >= len(program):
         return
 
-    inst, op1, op2 = decode(program[addr])
+    protected, inst, op1, op2 = decode(program[addr])
+
+    if protected:
+        raise RuntimeError('Trying to toggle protected instruction: '
+                           f'{program[addr]}')
 
     match inst:
         # For one-argument instructions, inc becomes dec, and all other
@@ -36,15 +41,15 @@ def tgl_inst(program, addr):
         case _:
             raise ValueError(program[addr])
 
-    program[addr][0] = new_inst
+    program[addr][1] = new_inst
 
 
-def execute(program):
+def execute(program, a=7):
     pc = 0
-    register = {'a': 7, 'b': 0, 'c': 0, 'd': 0}
+    register = {'a': a, 'b': 0, 'c': 0, 'd': 0}
 
     while 0 <= pc < len(program):
-        inst, op1, op2 = decode(program[pc])
+        protected, inst, op1, op2 = decode(program[pc])
         pc_add = 1
 
         match inst:
@@ -65,6 +70,14 @@ def execute(program):
             case 'tgl':
                 offset = op1 if type(op1) == int else register[op1]
                 tgl_inst(program, pc + offset)
+            case 'nop':
+                pass
+            case 'mul':
+                # Multiple reg-b * reg-d and store in reg-a; set reg-d = 0.
+                # NOTE: Not general purpose; super specific to part 2 of this
+                # problem.
+                register['a'] = register['b'] * register['d']
+                register['d'] = 0
             case _:
                 raise ValueError(program[pc])
 
@@ -79,7 +92,38 @@ def part1(program):
 
 
 def part2(program):
-    return None
+    # Insight:
+    # 3 cpy 0 a
+    # 4 cpy b c
+    # 5 inc a
+    # 6 dec c
+    # 7 jnz c -2
+    # 8 dec d
+    # 9 jnz d -5
+    # is equivalent to a <- b * d; cpy 0 d
+    # (as long as tgl doesn't modify the code)
+
+    # Hack to change the specific input I got; not generalized.
+    # Replace the expensive multiply loops with one 'mul' instruction and then
+    # pad with 'nop' instructions to keep the addresses correct. Lastly,
+    # protect these replacement instructions from 'tgl' changes.
+    if program[3:10] == [[False, 'cpy', 0, 'a'],
+                         [False, 'cpy', 'b', 'c'],
+                         [False, 'inc', 'a'],
+                         [False, 'dec', 'c'],
+                         [False, 'jnz', 'c', -2],
+                         [False, 'dec', 'd'],
+                         [False, 'jnz', 'd', -5]]:
+        program[3:10] = [[True, 'mul'],
+                         [True, 'nop'],
+                         [True, 'nop'],
+                         [True, 'nop'],
+                         [True, 'nop'],
+                         [True, 'nop'],
+                         [True, 'nop']]
+
+    register = execute(program, 12)
+    return register['a']
 
 
 def slurp(fname):
@@ -89,11 +133,13 @@ def slurp(fname):
 
 def parse(inp):
     program = []
+    protected = False  # is this instruction protected from tgl changes
     for line in inp:
         toks = line.split()
         for i in range(1, len(toks)):
             if toks[i].lstrip('-').isdigit():
                 toks[i] = int(toks[i])
+        toks.insert(0, protected)
         program.append(toks)
     return program
 
