b5716b52f869d3f69ecdf5e5901c45ed2e5fa15e
John <65102131+saaly182@users.noreply.github.com>
2023-02-24 21:33:29 -0800

    Soln 2016 d22 p2

--- 2016/d22/d22.py
+++ 2016/d22/d22.py
@@ -2,6 +2,7 @@
 
 import dataclasses
 import itertools
+import re
 
 
 @dataclasses.dataclass
@@ -11,6 +12,36 @@ class Device:
     used: int
     avail: int
     pct: int
+    x: int = dataclasses.field(init=False)
+    y: int = dataclasses.field(init=False)
+
+    def __post_init__(self):
+        mo = re.fullmatch(r'/dev/grid/node-x(\d+)-y(\d+)', self.name)
+        self.x = int(mo.group(1))
+        self.y = int(mo.group(2))
+
+
+def show_nodes(df):
+    """Visualize the nodes."""
+    nodes = {}
+    for device in df:
+        nodes[(device.x, device.y)] = device
+    maxx = max([x for x, _ in nodes])
+    maxy = max([y for _, y in nodes])
+
+    for y in range(maxy + 1):
+        for x in range(maxx + 1):
+            device = nodes[(x, y)]
+            if y == 0 and x == maxx:
+                ch = 'G'
+            elif device.used == 0:
+                ch = '_'
+            elif device.used > 400:
+                ch = '#'
+            else:
+                ch = '.'
+            print(ch, end='')
+        print()
 
 
 def part1(df):
@@ -19,12 +50,55 @@ def part1(df):
         for d1, d2 in ((a, b), (b, a)):
             if 0 < d1.used <= d2.avail:
                 viable_pairs.append((d1, d2))
-
     return len(viable_pairs)
 
 
-def part2():
-    return None
+def part2(df):
+    # Trying to do a BFS or DFS seems infeasible for this. Finally, scanning
+    # reddit, it turns out that the input data is highly specialized in that
+    # there's one empty node, and no other pair of nodes can accommodate data
+    # moves. That is, the only moves possible involve the currently empty node.
+    # There are also a number of very large nodes that simply cannot move.
+    # So I'm just printing out the grid graphically and solving by hand. My
+    # input looks like this:
+    #
+    # .............................G
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ........######################
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..........._..................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    # ..............................
+    #
+    # Manually determined the minimum moves: 188
+    #
+
+    show_nodes(df)
 
 
 def slurp(fname):
@@ -46,12 +120,13 @@ def parse(inp):
 
 
 def main():
+    sample_input = slurp('input/sample_input.txt')
     main_input = slurp('input/input.txt')
 
-    for inp in (main_input,):
+    for inp in (sample_input, main_input):
         df = parse(inp)
         print("Part 1 answer =", part1(df))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(df))
         print()
 
 
