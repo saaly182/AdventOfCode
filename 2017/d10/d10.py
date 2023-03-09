925d58ad37e9b825b9df39a1fa4ec81836b8bee3
John <65102131+saaly182@users.noreply.github.com>
2023-03-08 21:45:40 -0800

    Soln 2017 d10 p2

--- 2017/d10/d10.py
+++ 2017/d10/d10.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3 -u
 
+import functools
+
+
 def twist(lst, pos, length):
     """Reverse the slice at pos with length in list lst.
     Treat lst as circular. Modifies lst in place."""
@@ -21,7 +24,34 @@ def twist(lst, pos, length):
     lst[:length + pos - len_lst] = rev_tail_head[len_lst - pos:]
 
 
-def part1(lengths):
+def densify(sparse_hash):
+    dense_hash = []
+    for i in range(0, 256, 16):
+        dense_hash.append(functools.reduce(lambda x, y: x ^ y,
+                                           sparse_hash[i:i + 16]))
+    return dense_hash
+
+
+def hexify(lst):
+    return ''.join([f'{a:02x}' for a in lst])
+
+
+def knot_hash(s):
+    lengths = [ord(a) for a in s] + [17, 31, 73, 47, 23]
+    sparse_hash = list(range(256))
+    pos = 0
+    skip = 0
+    for _ in range(64):
+        for length in lengths:
+            twist(sparse_hash, pos, length)
+            pos += length + skip
+            skip += 1
+
+    return hexify(densify(sparse_hash))
+
+
+def part1(lengths_str):
+    lengths = tuple([int(a) for a in lengths_str.split(',')])
     x = list(range(256))
     pos = 0
     skip = 0
@@ -32,8 +62,8 @@ def part1(lengths):
     return x[0] * x[1]
 
 
-def part2():
-    return None
+def part2(s):
+    return knot_hash(s)
 
 
 def slurp(fname):
@@ -44,9 +74,8 @@ def slurp(fname):
 def main():
     main_input = slurp('input/input.txt')
     for inp in (main_input, ):
-        lengths = tuple([int(a) for a in inp[0].split(',')])
-        print("Part 1 answer =", part1(lengths))
-        print("Part 2 answer =", part2())
+        print("Part 1 answer =", part1(inp[0]))
+        print("Part 2 answer =", part2(inp[0]))
         print()
 
 
