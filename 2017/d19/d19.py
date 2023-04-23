0fbfb34365932f2fedbf310fd9b49977e4a9ea69
John <65102131+saaly182@users.noreply.github.com>
2023-04-22 21:56:25 -0700

    Switch to using ghost points to simplify the code

--- 2017/d19/d19.py
+++ 2017/d19/d19.py
@@ -5,11 +5,12 @@ sys.path.append('../../lib')
 import dirutils
 
 SPACE = ' '
+BOUNDARY = '@'
 
 
 def travel_path(diagram: tuple) -> tuple[str, int]:
-    r1 = 0
-    c1 = diagram[0].index('|')
+    r1 = 1
+    c1 = diagram[1].index('|')
     dirvec = dirutils.dirvecs['S']
     letters = []
     step_count = 1
@@ -18,15 +19,10 @@ def travel_path(diagram: tuple) -> tuple[str, int]:
     while not at_end:
         r2 = r1 + dirvec[0]
         c2 = c1 + dirvec[1]
-        try:
-            x = diagram[r2][c2]
-        except IndexError:
-            # this can happen if the path ends with a letter
-            if diagram[r1][c1].isalpha():
-                at_end = True
-                continue
-            else:
-                raise
+        x = diagram[r2][c2]
+        if x == BOUNDARY or x == SPACE:
+            at_end = True
+            continue
 
         step_count += 1
 
@@ -36,31 +32,18 @@ def travel_path(diagram: tuple) -> tuple[str, int]:
         elif x.isalpha():
             letters.append(x)
 
-            # peek at the next cell to check for end-of-path
-            r3 = r2 + dirvec[0]
-            c3 = c2 + dirvec[1]
-            try:
-                y = diagram[r3][c3]
-                if y == SPACE:
-                    at_end = True
-            except IndexError:
-                at_end = True
-
         elif x == '+':
             new_dirvec = None
             for uv in dirutils.unitvecs:
                 r3 = r2 + uv[0]
                 c3 = c2 + uv[1]
-                try:
-                    y = diagram[r3][c3]
-                except IndexError:
-                    continue
-                if y == SPACE:
+                y = diagram[r3][c3]
+                if y == BOUNDARY or y == SPACE:
                     continue
                 if dirvec[0] + uv[0] == 0 and dirvec[1] + uv[1] == 0:
                     # this is going back from where we came, so ignore
                     continue
-                assert new_dirvec is None  # otherwise there multiple exits
+                assert new_dirvec is None  # otherwise there are multiple exits
                 new_dirvec = uv
             if new_dirvec is None:
                 at_end = True
@@ -88,9 +71,12 @@ def slurp(fname: str) -> list[str]:
 
 
 def parse(inp: list) -> tuple:
-    diagram = []
+    # include ghost points around all edges
+    ghostrow = tuple(BOUNDARY * (len(inp[0]) + 2))
+    diagram = [ghostrow]
     for line in inp:
-        diagram.append(tuple(line))
+        diagram.append(tuple(BOUNDARY + line + BOUNDARY))
+    diagram.append(ghostrow)
     return tuple(diagram)
 
 
