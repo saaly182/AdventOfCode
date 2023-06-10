028287cdfc4f1c703fb6d48d4c4420c3c7ff803f
John <65102131+saaly182@users.noreply.github.com>
2023-06-09 21:28:36 -0700

    Soln 2018 d03

---
+++ 2018/d03/d03.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3 -u
+
+from collections import defaultdict
+from typing import DefaultDict, NamedTuple
+import re
+
+
+class Claim(NamedTuple):
+    id: int
+    c0: int
+    r0: int
+    width: int
+    height: int
+
+
+def mark_fabric(claims: tuple) -> DefaultDict[tuple, list]:
+    fabric = defaultdict(list)
+    for claim in claims:
+        for r in range(claim.r0, claim.r0 + claim.height):
+            for c in range(claim.c0, claim.c0 + claim.width):
+                fabric[(r, c)].append(claim.id)
+    return fabric
+
+
+def part1(fabric: DefaultDict[tuple, list]) -> int:
+    return sum([1 for cell, claim_ids in fabric.items() if len(claim_ids) > 1])
+
+
+def part2(fabric: DefaultDict[tuple, list]) -> int:
+    nonoverlap_claims = {cid for cids in fabric.values() for cid in cids}
+    for cell, claim_ids in fabric.items():
+        if len(claim_ids) > 1:
+            for cid in claim_ids:
+                if cid in nonoverlap_claims:
+                    nonoverlap_claims.remove(cid)
+    assert len(nonoverlap_claims) == 1
+    answer_claim, = nonoverlap_claims
+    return answer_claim
+
+
+def slurp(fname: str) -> list[str]:
+    with open(fname) as file:
+        return [line.rstrip() for line in file.readlines()]
+
+
+def parse(inp: list) -> tuple:
+    # example: #27 @ 928,149: 14x25
+    claims = []
+    pattern = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+    for line in inp:
+        if mo := pattern.fullmatch(line):
+            claims.append(Claim(*[int(x) for x in mo.groups()]))
+        else:
+            raise ValueError(f'bad input: {line}')
+    return tuple(claims)
+
+
+def main():
+    sample_input = slurp('input/sample_input.txt')
+    main_input = slurp('input/input.txt')
+
+    for inp in (sample_input, main_input):
+        claims = parse(inp)
+        fabric = mark_fabric(claims)
+        print("Part 1 answer =", part1(fabric))
+        print("Part 2 answer =", part2(fabric))
+        print()
+
+
+if __name__ == '__main__':
+    main()
