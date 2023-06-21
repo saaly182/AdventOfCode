6e1e196a5fd98890e4d7ca97e52c3a738001ee1e
John <65102131+saaly182@users.noreply.github.com>
2023-06-20 22:38:37 -0700

    Soln 2018 d08 p1

---
+++ 2018/d08/d08.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3 -u
+
+import collections
+
+
+class Node:
+    def __init__(self, nums: list):
+        # recursively creates child Nodes
+        self.id = len(nums)
+        num_children = nums.pop()
+        num_metadata = nums.pop()
+        self.children = []
+        self.metadata = []
+        for c in range(num_children):
+            self.children.append(Node(nums))
+        for m in range(num_metadata):
+            self.metadata.append(nums.pop())
+
+    def __repr__(self):
+        return f'Node({self.id=} {self.children=} {self.metadata=})'
+
+
+def part1(input_nums: tuple) -> int:
+    root = Node(list(reversed(input_nums)))
+    metadata_sum = 0
+    q = collections.deque([root])
+    while q:
+        node = q.popleft()
+        metadata_sum += sum(node.metadata)
+        q.extend(node.children)
+
+    return metadata_sum
+
+
+def part2():
+    return None
+
+
+def slurp(fname: str) -> list[str]:
+    with open(fname) as file:
+        return [line.rstrip() for line in file.readlines()]
+
+
+def main():
+    sample_input = slurp('input/sample_input.txt')
+    main_input = slurp('input/input.txt')
+
+    for inp in (sample_input, main_input):
+        input_nums = tuple([int(x) for x in inp[0].split()])
+        print("Part 1 answer =", part1(input_nums))
+        print("Part 2 answer =", part2())
+        print()
+
+
+if __name__ == '__main__':
+    main()
