8851d06f00bd41839c4193060dbacc2a9dd26cf3
John <65102131+saaly182@users.noreply.github.com>
2023-07-29 23:28:46 -0700

    Soln 2018 d14 p2

--- 2018/d14/d14.py
+++ 2018/d14/d14.py
@@ -11,7 +11,7 @@ def show(scores: list, e1: int, e2: int) -> None:
     print()
 
 
-def part1(rcount: int):
+def part1(rcount: int) -> str:
     scores = [3, 7]
     elf1 = 0
     elf2 = 1
@@ -24,15 +24,36 @@ def part1(rcount: int):
     return ''.join([str(d) for d in scores[rcount:rcount + 10]])
 
 
-def part2():
-    return None
+def part2(seq: str) -> int:
+    seqlen = len(seq)
+    found = False
+    scores = [3, 7]
+    elf1 = 0
+    elf2 = 1
+
+    while not found:
+        for d in [int(d) for d in str(scores[elf1] + scores[elf2])]:
+            scores.append(d)
+            if len(scores) >= seqlen:
+                tail = ''.join([str(d) for d in scores[-seqlen:]])
+                if tail == seq:
+                    found = True
+                    break
+        elf1 = (elf1 + 1 + scores[elf1]) % len(scores)
+        elf2 = (elf2 + 1 + scores[elf2]) % len(scores)
+
+    return len(scores) - seqlen
 
 
 def main():
     rcounts = (9, 5, 18, 2018, 30121)
     for rcount in rcounts:
         print(f"Part 1 answer for {rcount} =", part1(rcount))
-        print("Part 2 answer =", part2())
+        print()
+
+    seqs = ('51589', '01245', '92510', '59414', '030121')
+    for seq in seqs:
+        print(f"Part 2 answer for {seq} =", part2(seq))
         print()
 
 
