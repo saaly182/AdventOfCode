ceaf1acefad10061ec141054bd87bfc7d6ead237
John <65102131+saaly182@users.noreply.github.com>
2023-12-09 22:11:05 -0800

    Soln 2023 d01 p2

--- 2023/d01/d01.py
+++ 2023/d01/d01.py
@@ -6,7 +6,7 @@ import re
 def part1(inp: list[str]) -> int:
     calsum = 0
     for s in inp:
-        m = re.findall(r'[0-9]', s)
+        m = re.findall(r'[1-9]', s)
         if not m:
             raise ValueError(f'no digits in input: {s}')
         calibration = 10 * int(m[0]) + int(m[-1])
@@ -14,8 +14,24 @@ def part1(inp: list[str]) -> int:
     return calsum
 
 
-def part2():
-    return None
+def part2(inp: list[str]) -> int:
+    # Note that digit names can overlap, like "twone" is (2, 1).
+    calsum = 0
+    dnames = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6,
+              'seven': 7, 'eight': 8, 'nine': 9}
+    a = '[1-9]|'
+    b = '|'.join(dnames.keys())
+    dre_forward = a + b
+    dre_reverse = a + b[::-1]
+
+    for s in inp:
+        ds1 = re.search(dre_forward, s).group()
+        ds2 = re.search(dre_reverse, s[::-1]).group()[::-1]
+        d1 = int(ds1) if ds1 in '123456789' else dnames[ds1]
+        d2 = int(ds2) if ds2 in '123456789' else dnames[ds2]
+        calibration = 10 * d1 + d2
+        calsum += calibration
+    return calsum
 
 
 def slurp(fname: str) -> list[str]:
@@ -29,7 +45,7 @@ def main():
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(inp))
         print()
 
 
