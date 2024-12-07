cda45cd5988b99c4c9f6b6108a5c107fa54d4dd3
John <65102131+saaly182@users.noreply.github.com>
2024-12-06 23:12:50 -0800

    Soln 2024 d05 p2

--- 2024/d05/d05.py
+++ 2024/d05/d05.py
@@ -1,40 +1,71 @@
 #!/usr/bin/python3 -u
 
+import collections
+import graphlib
 import re
 
 
-def part1(rules: tuple, pagelists: tuple) -> int:
-    middles_sum = 0
+def classify_pagelists(rules: tuple, pagelists: tuple) -> tuple:
+    """
+    Return a tuple of 2-tuples where the 2-tuples are:
+    (pagelist, broken-rules)
+    """
+    results = []
 
     for pages in pagelists:
         pset = set(pages)
         # code assumes no dups in each list of pages
         assert len(pages) == len(pset)
 
-        # create hash of indexes
+        # create hash of indexes for this particular pagelist
         pidx = dict([(b, a) for a, b in enumerate(pages)])
 
-        prules = []  # rules for these pages
+        prules = []  # rules specific to these pages
+        broken_rules = []
         for r in rules:
             if r[0] in pset and r[1] in pset:
                 prules.append(r)
 
-        correct = True
-        for ra, rb in prules:
-            if pidx[ra] > pidx[rb]:
-                correct = False
-                break
+        for r in prules:
+            if pidx[r[0]] > pidx[r[1]]:
+                broken_rules.append(r)
 
-        if correct:
+        results.append((pages, tuple(broken_rules)))
+
+    return tuple(results)
+
+
+def part1(rules: tuple, pagelists: tuple) -> int:
+    middles_sum = 0
+    for pages, br in classify_pagelists(rules, pagelists):
+        if not br:  # if no broken rules
             assert len(pages) % 2 == 1
-            middle = pages[len(pages) // 2]  # don't forget zero-based indexes
+            middle = pages[len(pages) // 2]  # don't forget: zero-based indexes
             middles_sum += middle
-
     return middles_sum
 
 
-def part2():
-    return None
+def part2(rules: tuple, pagelists: tuple) -> int:
+    # TODO: refactor to only call classify_pagelists once
+    middles_sum = 0
+    for pages, br in classify_pagelists(rules, pagelists):
+        if not br:
+            # ignore pagelists that are already correctly ordered
+            continue
+
+        # topo sort this pagelist using only the applicable rules
+        pset = set(pages)
+        graph = collections.defaultdict(set)
+        for r in rules:
+            if r[0] in pset and r[1] in pset:
+                graph[r[1]].add(r[0])
+        ts = graphlib.TopologicalSorter(graph)
+        fixed_pages = tuple(ts.static_order())
+
+        assert len(fixed_pages) % 2 == 1
+        middle = fixed_pages[len(fixed_pages) // 2]
+        middles_sum += middle
+    return middles_sum
 
 
 def slurp(fname: str) -> tuple[str, ...]:
@@ -64,7 +95,7 @@ def main():
     for inp in (sample_input, main_input):
         rules, pagelists = parse_input(inp)
         print("Part 1 answer =", part1(rules, pagelists))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(rules, pagelists))
         print()
 
 
