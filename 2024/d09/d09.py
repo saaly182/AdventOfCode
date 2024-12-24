2be0262ff1f6f64b514bf80d59c52b10a63de2b4
John <65102131+saaly182@users.noreply.github.com>
2024-12-23 22:27:59 -0800

    Soln 2024 d09 p2

--- 2024/d09/d09.py
+++ 2024/d09/d09.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3 -u
 
+import re
+
+
 EMPTY_BLOCK = -1
 
 
-def frag(disk: list) -> None:
+def consolidate_freespace(disk: list) -> None:
     last_used = 0
     while True:
         first_empty = disk.index(EMPTY_BLOCK)
@@ -33,13 +36,45 @@ def show(disk: list) -> None:
     print(''.join(d))
 
 
+def index_free_slots(disk: tuple, f_start: int, f_size: int) -> tuple:
+    """Index empty slots before f_start and at least f_size"""
+    idx = []
+    diskstr = ''.join(['.' if x == EMPTY_BLOCK else 'F' for x in disk])
+    pat = r'\.' * f_size + r'+'
+    for mo in re.finditer(pat, diskstr):
+        if mo.start() < f_start:
+            e_start = mo.start()
+            e_size = mo.end() - mo.start()
+            idx.append((e_start, e_size))
+        else:
+            break
+    return tuple(idx)
+
+
+def fileinfo(disk: tuple, file_num: int) -> tuple[int, int]:
+    diskstr = ''.join(['F' if x == file_num else '*' for x in disk])
+    mo = re.search(r'F+', diskstr)
+    assert mo
+    return mo.start(), mo.end() - mo.start()
+
+
 def part1(disk: list) -> int:
-    frag(disk)
+    consolidate_freespace(disk)
     return fs_checksum(disk)
 
 
-def part2():
-    return None
+def part2(disk: list) -> int:
+    for file_num in range(max(disk), 0, -1):
+        f_start, f_size = fileinfo(tuple(disk), file_num)
+        free_slots = index_free_slots(tuple(disk), f_start, f_size)
+        for e_start, e_size in free_slots:
+            if e_size >= f_size and e_start < f_start:
+                # move file
+                disk[e_start:e_start + f_size] = [file_num] * f_size
+                # set file's original location to 'empty'
+                disk[f_start:f_start + f_size] = [EMPTY_BLOCK] * f_size
+                break
+    return fs_checksum(disk)
 
 
 def slurp(fname: str) -> list[int]:
@@ -66,8 +101,8 @@ def main():
     main_input = slurp('input/input.txt')
 
     for inp in (sample_input, main_input):
-        print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
+        print("Part 1 answer =", part1(inp[:]))
+        print("Part 2 answer =", part2(inp[:]))
         print()
 
 
