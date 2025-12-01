2663d96b79371d2c0ef5c374fbaffb642db6a4e1
John <65102131+saaly182@users.noreply.github.com>
2025-11-30 22:48:32 -0800

    Rename input parsing function in skel

--- lib/skel.py
+++ lib/skel.py
@@ -8,14 +8,14 @@ def part2() -> int:
     return -99
 
 
-def slurp(fname: str) -> tuple[str, ...]:
+def parse_input(fname: str) -> tuple[str, ...]:
     with open(fname) as file:
         return tuple(line.rstrip() for line in file.readlines())
 
 
 def main():
-    sample_input = slurp('input/sample_input.txt')
-    main_input = slurp('input/input.txt')
+    sample_input = parse_input('input/sample_input.txt')
+    main_input = parse_input('input/input.txt')
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1())
