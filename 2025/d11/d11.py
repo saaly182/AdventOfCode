7fee539e65471e1404b5605beb2419aa09b2b959
John <65102131+saaly182@users.noreply.github.com>
2026-01-19 17:26:21 -0800

    Soln 2025 d11 p2

--- 2025/d11/d11.py
+++ 2025/d11/d11.py
@@ -1,35 +1,59 @@
 #!/usr/bin/python3 -u
 
-from typing import Iterator
+from collections import defaultdict
 
 
-def dfs(graph: dict, this_node: str, dst: str, path=None) -> Iterator[tuple]:
+def dfs_path_count(graph: defaultdict[str, tuple], this_node: str, dst: str,
+                   path: list = None, cache: dict = None) -> int:
     if path is None:
         path = []
 
+    if cache is None:
+        cache = {}
+
+    cache_key = (this_node, dst)
+
+    if cache_key in cache:
+        return cache[cache_key]
+
+    path_count = 0
     path.append(this_node)
 
     if this_node == dst:
-        yield tuple(path)
+        path_count = 1
     else:
         for next_node in graph[this_node]:
             if next_node not in path:
-                yield from dfs(graph, next_node, dst, path)
+                path_count += dfs_path_count(graph, next_node, dst, path, cache)
                 if path:
                     path.pop()
 
-
-def part1(g: dict) -> int:
-    all_paths = tuple(dfs(g, 'you', 'out'))
-    return len(all_paths)
+    cache[cache_key] = path_count
+    return path_count
 
 
-def part2() -> int:
-    return -99
+def part1(g: defaultdict[str, tuple]) -> int:
+    return dfs_path_count(g, 'you', 'out')
 
 
-def parse_input(fname: str) -> dict:
-    graph = {}
+def part2(g: defaultdict[str, tuple]) -> int:
+    # svr -> dac -> fft -> out
+    x1 = dfs_path_count(g, "svr", "dac")
+    x2 = dfs_path_count(g, "dac", "fft")
+    x3 = dfs_path_count(g, "fft", "out")
+
+    # svr -> fft -> dac -> out
+    y1 = dfs_path_count(g, "svr", "fft")
+    y2 = dfs_path_count(g, "fft", "dac")
+    y3 = dfs_path_count(g, "dac", "out")
+
+    z = (x1 * x2 * x3) + (y1 * y2 * y3)
+
+    return z
+
+
+def parse_input(fname: str) -> defaultdict[str, tuple]:
+    graph = defaultdict(tuple)
     with open(fname) as file:
         for line in file:
             toks = line.rstrip().replace(':', '').split()
@@ -39,12 +63,14 @@ def parse_input(fname: str) -> dict:
 
 def main():
     sample_input = parse_input('input/sample_input.txt')
+    sample_input2 = parse_input('input/sample_input2.txt')
     main_input = parse_input('input/input.txt')
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
-        print()
+
+    for inp in (sample_input2, main_input):
+        print("Part 2 answer =", part2(inp))
 
 
 if __name__ == '__main__':
