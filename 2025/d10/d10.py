f0b795d561ead155e88ee7f733c674128861d4fb
John <65102131+saaly182@users.noreply.github.com>
2026-05-19 20:26:30 -0700

    2025 d10 p2 - technically correct but blows up on real input

--- 2025/d10/d10.py
+++ 2025/d10/d10.py
@@ -11,7 +11,7 @@ class Machine:
     jolts: tuple
 
 
-def min_start_pushes(m: Machine) -> int:
+def min_start_pushes_lights(m: Machine) -> int:
     def bfs():  # inner function
         nonlocal final_step
         while q:
@@ -56,15 +56,73 @@ def min_start_pushes(m: Machine) -> int:
     return len(bseq)
 
 
+def min_start_pushes_jolts(m: Machine) -> int:
+    def bfs():  # inner function
+        nonlocal final_step
+        while q:
+            button1, jolts1 = q.popleft()
+            jolts_next = list(jolts1)
+            for pos in button1:
+                jolts_next[pos] += 1
+            jolts2 = tuple(jolts_next)
+
+            if jolts2 == m.jolts:
+                final_step = (button1, jolts1)
+                return
+
+            # stop exploring this path if any jolt is now too large
+            jolts_ok = True
+            for i in range(len(m.jolts)):
+                if jolts2[i] > m.jolts[i]:
+                    jolts_ok = False
+                    break
+            if not jolts_ok:
+                continue
+
+            for b2 in m.buttons:
+                k2 = (b2, jolts2)
+                if k2 not in parent:
+                    parent[k2] = (button1, jolts1)
+                    q.append(k2)
+
+    q = collections.deque()
+    starting_jolts = tuple([0] * len(m.jolts))
+    parent = {}
+    final_step = None
+
+    for b1 in m.buttons:
+        k1 = (b1, starting_jolts)
+        parent[k1] = None
+        q.append(k1)
+
+    bfs()
+
+    # now reconstruct the button sequence
+    bseq = []
+    step = final_step
+    while True:
+        if step is None:
+            break
+        bu, li = step
+        bseq.append(bu)
+        step = parent[step]
+    bseq.reverse()
+
+    return len(bseq)
+
+
 def part1(machines) -> int:
     pushcount = 0
     for m in machines:
-        pushcount += min_start_pushes(m)
+        pushcount += min_start_pushes_lights(m)
     return pushcount
 
 
-def part2() -> int:
-    return -99
+def part2(machines) -> int:
+    pushcount = 0
+    for m in machines:
+        pushcount += min_start_pushes_jolts(m)
+    return pushcount
 
 
 def parse_input(fname: str) -> tuple[Machine, ...]:
@@ -99,7 +157,7 @@ def main():
 
     for inp in (sample_input, main_input):
         print("Part 1 answer =", part1(inp))
-        print("Part 2 answer =", part2())
+        print("Part 2 answer =", part2(inp))
         print()
 
 
