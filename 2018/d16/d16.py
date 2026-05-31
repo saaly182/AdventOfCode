d88e519d4eceef6eb39359805a3615de93b8e34e
John <65102131+saaly182@users.noreply.github.com>
2026-05-30 22:24:47 -0700

    Soln 2018 d16 p1

---
+++ 2018/d16/d16.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python3 -u
+
+import re
+import sys
+sys.path.append('../../lib')
+import multiline_record  # noqa: E402 F401
+
+
+class Device:
+    def __init__(self) -> None:
+        self.regcount = 4
+        self.registers = [0] * self.regcount
+        self.opnames = tuple(n for n in dir(self) if n.startswith('op_'))
+
+    def set_registers(self, registers: list) -> None:
+        assert len(registers) == self.regcount
+        self.registers = registers
+
+    def cmp_registers(self, registers: list) -> bool:
+        assert len(registers) == self.regcount
+        return self.registers == registers
+
+    def op_addr(self, instruction: tuple[int, ...]) -> None:
+        """(add register)
+        stores into register C the result of adding register A and
+        register B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] + self.registers[b]
+
+    def op_addi(self, instruction: tuple[int, ...]) -> None:
+        """(add immediate) stores into register C the result of adding
+        register A and value B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] + b
+
+    def op_mulr(self, instruction: tuple[int, ...]) -> None:
+        """(multiply register) stores into register C the result of multiplying
+        register A and register B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] * self.registers[b]
+
+    def op_muli(self, instruction: tuple[int, ...]) -> None:
+        """(multiply immediate) stores into register C the result of
+        multiplying register A and value B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] * b
+
+    def op_banr(self, instruction: tuple[int, ...]) -> None:
+        """(bitwise AND register) stores into register C the result of the
+        bitwise AND of register A and register B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] & self.registers[b]
+
+    def op_bani(self, instruction: tuple[int, ...]) -> None:
+        """(bitwise AND immediate) stores into register C the result of the
+        bitwise AND of register A and value B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] & b
+
+    def op_borr(self, instruction: tuple[int, ...]) -> None:
+        """(bitwise OR register) stores into register C the result of the
+        bitwise OR of register A and register B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] | self.registers[b]
+
+    def op_bori(self, instruction: tuple[int, ...]) -> None:
+        """(bitwise OR immediate) stores into register C the result of the
+        bitwise OR of register A and value B."""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a] | b
+
+    def op_setr(self, instruction: tuple[int, ...]) -> None:
+        """(set register) copies the contents of register A into register C.
+        (Input B is ignored.)"""
+        a, b, c = instruction[1:]
+        self.registers[c] = self.registers[a]
+
+    def op_seti(self, instruction: tuple[int, ...]) -> None:
+        """(set immediate) stores value A into register C.
+        (Input B is ignored.)"""
+        a, b, c = instruction[1:]
+        self.registers[c] = a
+
+    def op_gtir(self, instruction: tuple[int, ...]) -> None:
+        """(greater-than immediate/register) sets register C to 1 if value A
+        is greater than register B. Otherwise, register C is set to 0."""
+        a, b, c = instruction[1:]
+        self.registers[c] = 1 if a > self.registers[b] else 0
+
+    def op_gtri(self, instruction: tuple[int, ...]) -> None:
+        """(greater-than register/immediate) sets register C to 1 if register A
+        is greater than value B. Otherwise, register C is set to 0."""
+        a, b, c = instruction[1:]
+        self.registers[c] = 1 if self.registers[a] > b else 0
+
+    def op_gtrr(self, instruction: tuple[int, ...]) -> None:
+        """(greater-than register/register) sets register C to 1 if register A
+        is greater than register B. Otherwise, register C is set to 0."""
+        a, b, c = instruction[1:]
+        self.registers[c] = 1 if self.registers[a] > self.registers[b] else 0
+
+    def op_eqir(self, instruction: tuple[int, ...]) -> None:
+        """(equal immediate/register) sets register C to 1 if value A is equal
+        to register B. Otherwise, register C is set to 0."""
+        a, b, c = instruction[1:]
+        self.registers[c] = 1 if a == self.registers[b] else 0
+
+    def op_eqri(self, instruction: tuple[int, ...]) -> None:
+        """(equal register/immediate) sets register C to 1 if register A is
+        equal to value B. Otherwise, register C is set to 0."""
+        a, b, c = instruction[1:]
+        self.registers[c] = 1 if self.registers[a] == b else 0
+
+    def op_eqrr(self, instruction: tuple[int, ...]) -> None:
+        """(equal register/register) sets register C to 1 if register A is
+        equal to register B. Otherwise, register C is set to 0."""
+        a, b, c = instruction[1:]
+        self.registers[c] = 1 if self.registers[a] == self.registers[b] else 0
+
+
+def part1(samples: tuple) -> int:
+    found3plus = 0
+    d = Device()
+
+    for before, instruction, after in samples:
+        op_success = 0
+        for op in d.opnames:
+            fnc = getattr(d, op)
+            d.set_registers(list(before))
+            fnc(instruction)
+            if d.cmp_registers(list(after)):
+                op_success += 1
+
+        if op_success >= 3:
+            found3plus += 1
+
+    return found3plus
+
+
+def part2() -> int:
+    return -99
+
+
+def parse_input(fname: str) -> tuple[tuple, tuple]:
+    samples = []
+    test_program = []
+
+    sample_re = re.compile(r'Before:\s+\[(\d+), (\d+), (\d+), (\d+)]\n'
+                           r'(\d+) (\d+) (\d+) (\d+)\n'
+                           r'After:\s+\[(\d+), (\d+), (\d+), (\d+)]\n')
+
+    test_program_re = re.compile(r'(\d+ \d+ \d+ \d+\n)+')
+
+    for record in multiline_record.multiline_file(fname):
+        if mo := sample_re.fullmatch(record):
+            nums = [int(x) for x in mo.groups()]
+            before = tuple(nums[:4])
+            instruction = tuple(nums[4:8])
+            after = tuple(nums[8:])
+            samples.append((before, instruction, after))
+        elif test_program_re.fullmatch(record):
+            for program_line in record.strip().split('\n'):
+                test_program.append(tuple(int(x) for x in program_line.split()))
+        else:
+            raise ValueError(f'Invalid input: {record}')
+
+    return tuple(samples), tuple(test_program)
+
+
+def main():
+    sample_input = parse_input('input/sample_input.txt')
+    main_input = parse_input('input/input.txt')
+
+    for samples, test_program in (sample_input, main_input):
+        print("Part 1 answer =", part1(samples))
+        print("Part 2 answer =", part2())
+        print()
+
+
+if __name__ == '__main__':
+    main()
